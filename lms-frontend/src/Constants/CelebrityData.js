c344fe8a8ac6b6b1e33384ed7f33fe84708058d6
arpana2003 <kedarnathrai019@gmail.com>
2025-04-15 22:26:17 +0530

    Updated About Us Page

--- lms-frontend/src/Constants/CelebrityData.js
+++ lms-frontend/src/Constants/CelebrityData.js
@@ -1,28 +1,31 @@
-import aboutMainImage from "../assets/Images/images3.jpeg";
+import Abdul_Kalam from "../assets/Images/Abdul Kalam.jpg";
+import Bill_Gates from "../assets/Images/Bill Gates.jpg";
+import Nelson_Mandela from "../assets/Images/Nelson Mandela.jpg";
+import Sundar_Pichai from "../assets/Images/Sundar Pichai.jpg";
 
 const celebrities = [
     {
         title:"Nelson Mandela",
         description: "Education is the most powerful tool you can use to change the world",
-        image:aboutMainImage ,
+        image:Nelson_Mandela ,
         slideNumber: 1 ,
     },
     {
         title:"Abdul Kalam",
         description: "Don't take rest after your first victory because if you fail in second, more lips are waiting to say that your first victory was just luck.",
-        image:aboutMainImage ,
+        image:Abdul_Kalam ,
         slideNumber: 2 ,
     },
     {
         title:"Bill Gates",
         description: "It's fine to celebrate success, but it is more important to heed the lessons of failure.",
-        image:aboutMainImage ,
+        image:Bill_Gates ,
         slideNumber: 3 ,
     },
     {
         title:"Sundar Pichai",
         description: "Wear your failure as a badge of honor.",
-        image:aboutMainImage ,
+        image:Sundar_Pichai ,
         slideNumber: 4 ,
     },
 ]
